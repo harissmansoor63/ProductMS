7118b47cec99b5fbb1e1b298497f32630bd6d713
“harissmansoor63” <hariss.mansoor@square63.org>
2021-04-08 00:04:36 +0500

    Signup task completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, presence: true 
-  validates :password, length: {minimum: 8}, format: { with: /(?=.*[a-z])/}
-   # /(?=.*[A-Z])/ /(?=.*\d)/ /(?=.*\W])/ 
+  validates :password, length: {minimum: 8}, format: { with: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}/}
+ 
+# the .{8,} does the same as length: {minimum: 8} 
+# /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W])/ 
+# /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}/
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
