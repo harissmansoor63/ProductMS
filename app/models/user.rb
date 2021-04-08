e78975412b18bd035abb477fdee62c70a541240b
“harissmansoor63” <hariss.mansoor@square63.org>
2021-04-08 18:28:10 +0500

    Sign up all subtasks complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,44 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, presence: true 
-  validates :password, length: {minimum: 8}, format: { with: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}/}
+  validates :password, format: { with: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}/}
  
 # the .{8,} does the same as length: {minimum: 8} 
 # /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W])/ 
 # /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}/
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+
+  validate :password_lower_case
+  validate :password_uppercase
+  validate :password_special_char
+  validate :password_contains_number
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+ def password_uppercase
+    return if !!password.match(/\p{Upper}/)
+    errors.add :password, ' must contain at least 1 uppercase '
+  end
+
+  def password_lower_case
+    return if !!password.match(/\p{Lower}/)
+    errors.add :password, ' must contain at least 1 lowercase '
+  end
+
+  def password_special_char
+    special = "?<>',?[]}{=-)(*&^%$#`~{}!"
+    regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+    return if password =~ regex
+    errors.add :password, ' must contain special character'
+  end
+
+  def password_contains_number
+    return if password.count("0-9") > 0
+    errors.add :password, ' must contain at least one number'
+  end
 
 end
