b99328f9b34ef948af70387a146ebc66b4e63b48
“harissmansoor63” <hariss.mansoor@square63.org>
2021-04-11 21:47:03 +0500

    Signup task after PR Changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,17 @@
 class User < ApplicationRecord
+
+  Special = "?<>',?[]}{=-)(*&^%$#`~{}!"
+  Regex = /[#{Special.gsub(/./){|char| "\\#{char}"}}]/
+
   validates :first_name, :last_name, presence: true 
   validates :password, format: { with: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}/}
- 
-# the .{8,} does the same as length: {minimum: 8} 
-# /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W])/ 
-# /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}/
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-
-  validate :password_lower_case
-  validate :password_uppercase
-  validate :password_special_char
-  validate :password_contains_number
-
+  validate :password_lower_case, :password_uppercase, :password_special_char, :password_contains_number
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
-
- def password_uppercase
+  def password_uppercase
     return if !!password.match(/\p{Upper}/)
     errors.add :password, ' must contain at least 1 uppercase '
   end
@@ -29,10 +21,8 @@ class User < ApplicationRecord
     errors.add :password, ' must contain at least 1 lowercase '
   end
 
-  def password_special_char
-    special = "?<>',?[]}{=-)(*&^%$#`~{}!"
-    regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
-    return if password =~ regex
+  def password_special_char 
+    return if password =~ Regex
     errors.add :password, ' must contain special character'
   end
 
