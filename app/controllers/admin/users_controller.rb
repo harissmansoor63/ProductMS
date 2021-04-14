d652e45bf698ecc023b286bfd5e2c8c2fcd0c0aa
“harissmansoor63” <hariss.mansoor@square63.org>
2021-04-14 17:32:14 +0500

    Listing Users Complete

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,28 +6,28 @@ class Admin::UsersController < ApplicationController
 		@users= User.where(role: User.roles[:user])
 	end
 
-	# def show
-	# 	@user = User.find(params[:id])
-	# end
+	def show
+		@user = User.find(params[:id])
+	end
 
-	# def edit
-	# 	@user = User.find(params[:id])
-	# end
+	def edit
+		@user = User.find(params[:id])
+	end
 
-	# def update
-	# 	@user = User.find(params[:id])
-	# 	if @user.update(user_params)
-	# 		redirect_to @user
-	# 	else
-	# 		render 'edit'
-	# 	end
-	# end
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
+	end
 
-	# def destroy
-	# 	@user = User.find(params[:id])
-	# 	@user.destroy
-	# 	redirect_to root_path
-	# end
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to root_path
+	end
 
 	private
 
