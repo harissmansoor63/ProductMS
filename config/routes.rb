4ed616603cde62a3eb5fba64c5815473faabecd9
“harissmansoor63” <hariss.mansoor@square63.org>
2021-04-14 12:52:14 +0500

    minor change in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  devise_for :users
+	get 'home/index'
+	devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+  	resources :users
+  end
+
+
+	# get 'users/set_password'
+
+	# get 'set_password/:id', to: 'users#set_password', as: :set_password
+
+	get 'users/:id/set_password', to: 'users#set_password', as: :set_password
+
+	put 'users/:id/update_set_password', to: 'users#update_set_password', as: :update_set_password 
+
+
+	# get 'users/:id/set_password', to: 'users#set_password'
+
+	# get 'setpassword', to: 'devise_invitable/registrations#update'
+
 
   root to: "home#index"
 end
