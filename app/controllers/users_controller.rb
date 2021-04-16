59f32f3ce7063cb183cf4ba3d7040fe2fcc14178
“harissmansoor63” <hariss.mansoor@square63.org>
2021-04-16 15:31:12 +0500

    correcting indendation issues 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
-	before_action :get_user
+  before_action :get_user
 
-	def set_password; end
+  def set_password; end
 
-	def update_set_password
-		if @user.update(user_password_params)
-			redirect_to root_path
-		else
-			render :set_password
-		end
-	end
+  def update_set_password
+    if @user.update(user_password_params)
+      redirect_to root_path
+    else
+      render :set_password
+    end
+  end
 
-	private
+  private
 
-	def user_password_params
-		params.require(:user).permit(:password, :password_confirmation)
-	end
+  def user_password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 
-	def get_user
-		@user = User.find(params[:id])
-	end
+  def get_user
+    @user = User.find(params[:id])
+  end
 end
