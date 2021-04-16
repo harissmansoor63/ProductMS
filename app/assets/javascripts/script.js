b9c28bf4cf9c44cab737276594572e3ea2a89cf8
“harissmansoor63” <hariss.mansoor@square63.org>
2021-04-16 16:40:45 +0500

    PR corrections

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,5 +3,5 @@ $(document).on('ready turbolinks:load', () => {scrollerSignUp();})
 function scrollerSignUp() {
   $('#productscrolldown').on('click', function() {
     $('#scroller').removeClass('d-none'); 
-  })
+  });
 }
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_action :get_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users= User.staff
+    @users = User.staff
   end
 
   def show; end
