d9b34fe59bf247d5280c138b298596507db2035a
“harissmansoor63” <hariss.mansoor@square63.org>
2021-04-20 16:14:56 +0500

    adding dropdown

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
-  
+  validates :title, :price, :description, :status, presence: true 
+  # validates :status, inclusion: {in: %w(Publish Draft Pending), message: "%{value} is not a valid blah blah"}
+  STATUS_ENUM = { Publish: 1, Draft: 2, Pending: 3 } 
+  enum status: STATUS_ENUM
 end
