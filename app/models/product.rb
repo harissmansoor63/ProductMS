a2dcc3a35eb80755c0dc44a6550488d4dcef2a84
“harissmansoor63” <hariss.mansoor@square63.org>
2021-04-28 12:41:29 +0500

    start

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,12 @@
 class Product < ApplicationRecord
   has_one_attached :image
-  validates :title, :price, :description, :status, presence: true 
+
+  validates :title, :price, :description, :status, presence: true
+
   STATUS_ENUM = { Publish: 1, Draft: 2, Pending: 3 } 
+
   enum status: STATUS_ENUM
+
   HEADERS = ['Title', 'Price', 'Status', 'Description']
 
   def self.search(search)
